e9a43c85d8e8a1afe440e97b4567ceef262039b6
Nuno Barros <barros@lip.pt>
2024-04-05 08:31:20 +0100

    Preparing configuration scripts for cibmodules

--- python/cibmodules/cibmodulesapp_gen.py
+++ python/cibmodules/cibmodulesapp_gen.py
@@ -4,31 +4,194 @@
 # Set moo schema search path                                                                              
 from dunedaq.env import get_moo_model_path
 import moo.io
+from asyncio.__main__ import console
 moo.io.default_load_path = get_moo_model_path()
 
 # Load configuration types                                                                                
 import moo.otypes
 moo.otypes.load_types("cibmodules/cibmodule.jsonnet")
+moo.otypes.load_types('readoutlibs/readoutconfig.jsonnet')
 
 import dunedaq.cibmodules.cibmodule as cibmodule
+import dunedaq.readoutlibs.readoutconfig as rconf
 
 from daqconf.core.app import App, ModuleGraph
 from daqconf.core.daqmodule import DAQModule
 #from daqconf.core.conf_utils import Endpoint, Direction
+from daqconf.core.conf_utils import Direction, Queue
 
-def get_cibmodules_app(nickname, num_cibmodules, some_configured_value, host="localhost"):
+
+# This is where things are a bit different from the CTB, as the CIB has a single trigger type
+# On the other hand, it has multiple instances running at the same time.
+# We will want to differentiate them
+
+def get_cib_hsi_app(
+        cib_hsi,
+        nickname,
+        LLT_SOURCE_ID,
+        HLT_SOURCE_ID,
+        QUEUE_POP_WAIT_MS=10,
+        LATENCY_BUFFER_SIZE=100000,
+        DATA_REQUEST_TIMEOUT=1000,
+):
+    '''
+    Here an entire application controlling one CTB board is generated. 
+    '''
+
+    # Temp variables - Remove
+    HOST=ctb_hsi.host_ctb_hsi
+    HLT_LIST=ctb_hsi.hlt_triggers
+    BEAM_LLT_LIST=ctb_hsi.beam_llt_triggers
+    CRT_LLT_LIST=ctb_hsi.crt_llt_triggers
+    PDS_LLT_LIST=ctb_hsi.pds_llt_triggers
+    FAKE_TRIG_1=ctb_hsi.fake_trig_1
+    FAKE_TRIG_2=ctb_hsi.fake_trig_2
+
+    console = Console()
+
+    # Define modules
+
+    modules = []
+    lus = []
+    # Prepare standard config with no additional configuration
+    console.log('generating DAQ module')
+
+    # Get default LLT and HLTs
+    hlt_trig = ctb.Hlt().pod()
+    beam_trig = ctb.Beam().pod()
+    crt_trig = ctb.Crt().pod()
+    pds_trig = ctb.Pds().pod()
+    fake_triggers = ctb.Misc().pod()
+
+    # Update LLT, HLTs with new or redefined triggers
+    updated_hlt_triggers = update_triggers(updated_triggers=HLT_LIST, default_trigger_conf=hlt_trig["trigger"])
+    updated_beam_triggers = update_triggers(updated_triggers=BEAM_LLT_LIST, default_trigger_conf=beam_trig["triggers"])
+    updated_crt_triggers = update_triggers(updated_triggers=CRT_LLT_LIST, default_trigger_conf=crt_trig["triggers"])
+    updated_pds_triggers = update_triggers(updated_triggers=PDS_LLT_LIST, default_trigger_conf=pds_trig["triggers"])
+
+    # Accept top config level fake trigger definition
+    fake_trig_1 = fake_triggers["randomtrigger_1"]
+    fake_trig_2 = fake_triggers["randomtrigger_2"]
+    if FAKE_TRIG_1 is not None:
+        fake_trig_1 = FAKE_TRIG_1
+    if FAKE_TRIG_2 is not None:
+        fake_trig_2 = FAKE_TRIG_2
+
+
+    modules += [DAQModule(name = nickname, 
+                          plugin = 'CTBModule',
+                          conf = ctb.Conf(board_config=ctb.Board_config(ctb=ctb.Ctb(misc=ctb.Misc(randomtrigger_1=fake_trig_1, randomtrigger_2=fake_trig_2),
+                                HLT=ctb.Hlt(trigger=updated_hlt_triggers),
+                                subsystems=ctb.Subsystems(pds=ctb.Pds(triggers=updated_pds_triggers),
+                                                          crt=ctb.Crt(triggers=updated_crt_triggers),
+                                                          beam=ctb.Beam(triggers=updated_beam_triggers)),
+                                sockets=ctb.Sockets(receiver=ctb.Receiver(host=HOST)) 
+                                )))
+                             )]
+
+
+    modules += [DAQModule(name = f"ctb_llt_datahandler",
+                        plugin = "HSIDataLinkHandler",
+                        conf = rconf.Conf(readoutmodelconf = rconf.ReadoutModelConf(source_queue_timeout_ms = QUEUE_POP_WAIT_MS, 
+                                                                                    source_id=LLT_SOURCE_ID,
+                                                                                    send_partial_fragment_if_available = True),
+                                          latencybufferconf = rconf.LatencyBufferConf(latency_buffer_size = LATENCY_BUFFER_SIZE),
+                                          rawdataprocessorconf = rconf.RawDataProcessorConf(source_id=LLT_SOURCE_ID),
+                                          requesthandlerconf= rconf.RequestHandlerConf(latency_buffer_size = LATENCY_BUFFER_SIZE,
+                                                                                          pop_limit_pct = 0.8,
+                                                                                          pop_size_pct = 0.1,
+                                                                                          source_id=LLT_SOURCE_ID,
+                                                                                          # output_file = f"output_{idx + MIN_LINK}.out",
+                                                                                          request_timeout_ms = DATA_REQUEST_TIMEOUT,
+                                                                                          warn_about_empty_buffer = False,
+                                                                                          enable_raw_recording = False)
+                                             ))]
+                                             
+    modules += [DAQModule(name = f"ctb_hlt_datahandler",
+        plugin = "HSIDataLinkHandler",
+        conf = rconf.Conf(readoutmodelconf = rconf.ReadoutModelConf(source_queue_timeout_ms = QUEUE_POP_WAIT_MS,
+                                                                source_id=HLT_SOURCE_ID,
+                                                                send_partial_fragment_if_available = True),
+                        latencybufferconf = rconf.LatencyBufferConf(latency_buffer_size = LATENCY_BUFFER_SIZE),
+                        rawdataprocessorconf = rconf.RawDataProcessorConf(source_id=HLT_SOURCE_ID),
+                        requesthandlerconf= rconf.RequestHandlerConf(latency_buffer_size = LATENCY_BUFFER_SIZE,
+                                                                        pop_limit_pct = 0.8,
+                                                                        pop_size_pct = 0.1,
+                                                                        source_id=HLT_SOURCE_ID,
+                                                                        # output_file = f"output_{idx + MIN_LINK}.out",
+                                                                        request_timeout_ms = DATA_REQUEST_TIMEOUT,
+                                                                        warn_about_empty_buffer = False,
+                                                                        enable_raw_recording = False)
+                        ))]
+
+    queues = [Queue(f"{nickname}.cib_iols_output",f"cib_datahandler.raw_input","HSIFrame",f'cib_link', 100000)]
+
+    mgraph = ModuleGraph(modules, queues=queues)
+    
+    mgraph.add_fragment_producer(id = LLT_SOURCE_ID, subsystem = "HW_Signals_Interface",
+                                         requests_in   = f"cib_datahandler.request_input",
+                                         fragments_out = f"cib_datahandler.fragment_queue")
+
+    mgraph.add_endpoint(f"timesync_cib_iols_output", f"cib_datahandler.timesync_output", "TimeSync", Direction.OUT, is_pubsub=True, toposort=False)
+
+    mgraph.add_endpoint("hsievents", f"{nickname}.hsievents", "HSIEvent",    Direction.OUT)
+
+    # dummy subscriber
+    mgraph.add_endpoint(None, None, data_type="TimeSync", inout=Direction.IN, is_pubsub=True)
+
+    console.log('generated DAQ module')
+    cib_app = App(modulegraph=mgraph, host=HOST, name=nickname)
+
+    return cib_app
+
+
+def get_cibmodules_app(nickname, num_cibmodules, source_id, host="localhost"):
     """
     Here the configuration for an entire daq_application instance using DAQModules from cibmodules is generated.
     """
-
+    
+    # NFB: Note that we are instantiating two separate modules
+    # The main issue is how to define the host of each module? Shall we simply assume that
+    # they both run on the same host and just define two different consecutive ports?
+    HOST=cib_hsi.host_cib_hsi
+    PORT=cib_hsi.host_cib_hsi
+    
+    # Grab 
+    console= Console()
+    
+    #Define the modules
     modules = []
 
+    # Prepare standard config with no additional configuration
+    console.log('generating DAQ module')
+
     for i in range(num_cibmodules):
-        modules += [DAQModule(name = f"nickname{i}", 
+        modules += [DAQModule(name = f"{nickname}_{i}", 
                               plugin = "CIBModule", 
-                              conf = cibmodule.Conf(some_configured_value = some_configured_value
+                              # The configuration should follow the 
+                              conf = cibmodule.Conf(board_config=cibmodule.Board_config(cib=cibmodule.CIB(misc=cibmodule.Misc(trigger_stream_enable=True)
+                                                                                                          sockets=cibmodule.Sockets(receiver=cibmodule.Receiver(host="127.0.0.1",
+                                                                                                                                                                port=4242
+                                                                                                              ))))
                                 )
                     )]
+
+    modules += [DAQModule(name = f"cib_datahandler",
+                        plugin = "HSIDataLinkHandler",
+                        conf = rconf.Conf(readoutmodelconf = rconf.ReadoutModelConf(source_queue_timeout_ms = QUEUE_POP_WAIT_MS, 
+                                                                                    source_id=source_id,
+                                                                                    send_partial_fragment_if_available = True),
+                                          latencybufferconf = rconf.LatencyBufferConf(latency_buffer_size = LATENCY_BUFFER_SIZE),
+                                          rawdataprocessorconf = rconf.RawDataProcessorConf(source_id=source_id),
+                                          requesthandlerconf= rconf.RequestHandlerConf(latency_buffer_size = LATENCY_BUFFER_SIZE,
+                                                                                          pop_limit_pct = 0.8,
+                                                                                          pop_size_pct = 0.1,
+                                                                                          source_id=source_id,
+                                                                                          # output_file = f"output_{idx + MIN_LINK}.out",
+                                                                                          request_timeout_ms = DATA_REQUEST_TIMEOUT,
+                                                                                          warn_about_empty_buffer = False,
+                                                                                          enable_raw_recording = False)
+                                             ))]
 
     mgraph = ModuleGraph(modules)
     cibmodules_app = App(modulegraph = mgraph, host = host, name = nickname)
--- schema/cibmodules/confgen.jsonnet
+++ schema/cibmodules/confgen.jsonnet
@@ -1,54 +1,52 @@
 // This is the configuration schema for cibmodules
 
+
+//local sdc = import "daqconf/confgen.jsonnet";
+//local daqconf = moo.oschema.hier(sdc).dunedaq.daqconf.confgen;
+
 local moo = import "moo.jsonnet";
-local sdc = import "daqconf/confgen.jsonnet";
-local daqconf = moo.oschema.hier(sdc).dunedaq.daqconf.confgen;
+
+local stypes = import "daqconf/types.jsonnet";
+local types = moo.oschema.hier(stypes).dunedaq.daqconf.types;
+
+local sboot = import "daqconf/bootgen.jsonnet";
+local bootgen = moo.oschema.hier(sboot).dunedaq.daqconf.bootgen;
+
+local cibmod = import "cibmodules/cibmodule.jsonnet";
+local cibgen = moo.oschema.hier(cibmod).dunedaq.cibmodules.cibmodule;
 
 local ns = "dunedaq.cibmodules.confgen";
 local s = moo.oschema.schema(ns);
 
 
-//local cs = {
-//
-//    int4 :    s.number(  "int4",    "i4",          doc="A signed integer of 4 bytes"),
-//    uint4 :   s.number(  "uint4",   "u4",          doc="An unsigned integer of 4 bytes"),
-//    int8 :    s.number(  "int8",    "i8",          doc="A signed integer of 8 bytes"),
-//    uint8 :   s.number(  "uint8",   "u8",          doc="An unsigned integer of 8 bytes"),
-//    float4 :  s.number(  "float4",  "f4",          doc="A float of 4 bytes"),
-//    double8 : s.number(  "double8", "f8",          doc="A double of 8 bytes"),
-//    boolean:  s.boolean( "Boolean",                doc="A boolean"),
-//    string:   s.string(  "String",   		   doc="A string"),   
-//    monitoring_dest: s.enum(     "MonitoringDest", ["local", "cern", "pocket"]),
-//
-//    cibmodules: s.record("cibmodules", [
-//        s.field( "some_configured_value", self.int4, default=31415, doc="A value which configures the CIBModule DAQModule instance"),
-//        s.field( "num_cibmodules", self.int4, default=2, doc="A value which configures the number of instances of CIBModule"),
-//    ]),
-//
-//    cibmodules_gen: s.record("cibmodules_gen", [
-//        s.field("boot", daqconf.boot, default=daqconf.boot, doc="Boot parameters"),
-//        s.field("cibmodules", self.cibmodules, default=self.cibmodules, doc="cibmodules parameters"),
-//    ]),
-//};
-//
 local cs = {
 
-    id :    s.number(  "int4",    "i4",          doc="IoLS ID"),
-    enable:  s.boolean( "Boolean",                doc="Enable this IoLS system"),
-    description:   s.string(  "String",   		   doc="Description of the IoLS system"),   
-    monitoring_dest: s.enum(     "MonitoringDest", ["local", "cern", "pocket"]),
+	 cibmod: s.record('cibmod', [
+		s.field('wibserver',    self.wiblist,               default=[],          doc='TESTSTAND tcp://192.168.121.1:1234'),
+    s.field('protowib',     self.wiblist,               default=[],          doc='TESTSTAND 192.168.121.1'),
+    s.field('host_wib',     types.host,                 default='localhost', doc='Host to run the WIB sw app on'),
+    s.field('gain',         self.gain_selector,         default=0,           doc='Channel gain selector: 14, 25, 7.8, 4.7 mV/fC (0 - 3)'),
+    s.field('shaping_time', self.shaping_time_selector, default=3,           doc='Channel peak time selector: 1.0, 0.5, 3, 2 us (0 - 3)'),
+    s.field('baseline',     self.baseline_selector,     default=2,           doc='Baseline selector: 0 (900 mV), 1 (200 mV), 2 (200 mV collection, 900 mV induction)'),
+    s.field('pulse_dac',    self.pulse_dac_selector,    default=0,           doc='Pulser DAC setting [0-63]'),
+    s.field('pulser',       types.flag,                 default=false,       doc="Switch to enable pulser"),
+    s.field('gain_match',   types.flag,                 default=true,        doc="Switch to enable gain matching for pulser amplitude"),
+    s.field('buffering',    self.buffering_selector,    default=0,           doc='0 (no buffer), 1 (se buffer), 2 (sedc buffer)'),
+    s.field('detector_type',self.detector_type_selector,default=0,           doc='Detector type: 0 (use WIB default), 1 (upper APA), 2 (lower APA), 3 (CRP)'),
+  ]),
+	
 
-    cibmodules: s.record("cibmodules", [
-        s.field( "some_configured_value", self.int4, default=31415, doc="A value which configures the CIBModule DAQModule instance"),
-        s.field( "num_cibmodules", self.int4, default=2, doc="A value which configures the number of instances of CIBModule"),
-    ]),
-
+	cibmodseq; s.sequence('cibs',self.cibmod, doc="Multiple CIB modules"),
+	
     cibmodules_gen: s.record("cibmodules_gen", [
-        s.field("boot", daqconf.boot, default=daqconf.boot, doc="Boot parameters"),
-        s.field("cibmodules", self.cibmodules, default=self.cibmodules, doc="cibmodules parameters"),
+        s.field("boot", bootgen.boot, default=bootgen.boot, doc="Boot parameters"),
+        s.field("num_cib_modules", types.int4, default=2, doc="Number of CIB modules"),
+        s.field("cib_modules",self.cibmodseq, )
+        
     ]),
 };
 
 
 // Output a topologically sorted array.
-sdc + moo.oschema.sort_select(cs, ns)
+//sdc + moo.oschema.sort_select(cs, ns)
+stypes + sboot + moo.oschema.sort_select(cs, ns)
