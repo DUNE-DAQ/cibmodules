377c218339ff58a7685370b0df1e03fd29565591
Your Name <you@example.com>
2026-03-02 13:00:15 -0600

    JCF: second pass from GitHub Copilot after I tell it that it's coverage according to lcov was very lacking. Sadly, this only brings us to 15% or so

--- unittest/CIB_test.cxx
+++ unittest/CIB_test.cxx
@@ -12,6 +12,8 @@
 
 #include "cib_utilities.h"
 
+#include <cstdint>
+
 #include "boost/test/unit_test.hpp"
 
 BOOST_AUTO_TEST_SUITE(CIB_test)
@@ -21,6 +23,44 @@ BOOST_AUTO_TEST_CASE(BitmaskHandlesBounds)
   BOOST_REQUIRE_EQUAL(dunedaq::cibmodules::util::bitmask(7, 0), 0xFFU);
   BOOST_REQUIRE_EQUAL(dunedaq::cibmodules::util::bitmask(0, 7), 0xFFU);
   BOOST_REQUIRE_EQUAL(dunedaq::cibmodules::util::bitmask(5, 3), 0x38U);
+  BOOST_REQUIRE_EQUAL(dunedaq::cibmodules::util::bitmask(31, 31), 0x80000000U);
+  BOOST_REQUIRE_EQUAL(dunedaq::cibmodules::util::bitmask(31, 0), 0xFFFFFFFFU);
+}
+
+BOOST_AUTO_TEST_CASE(CastToSignedHandlesPositiveAndNegative)
+{
+  constexpr std::uint32_t m1_mask = dunedaq::cib::daq::iols_trigger_t::bitmask_m1;
+
+  BOOST_REQUIRE_EQUAL(dunedaq::cibmodules::util::cast_to_signed(0x000123U, m1_mask), 0x123);
+  BOOST_REQUIRE_EQUAL(dunedaq::cibmodules::util::cast_to_signed(0x3FFFFFU, m1_mask), -1);
+  BOOST_REQUIRE_EQUAL(dunedaq::cibmodules::util::cast_to_signed(0x200001U, m1_mask), -2097151);
+}
+
+BOOST_AUTO_TEST_CASE(GetMHelpersDecodeTriggerFields)
+{
+  dunedaq::cib::daq::iols_trigger_t t{};
+
+  t.pos_m1 = 0x3FFFFF;
+  BOOST_REQUIRE_EQUAL(dunedaq::cibmodules::util::get_m1(t), -1);
+
+  t.pos_m1 = 0x000123;
+  BOOST_REQUIRE_EQUAL(dunedaq::cibmodules::util::get_m1(t), 0x123);
+
+  t.pos_m3 = 0x1FFFF;
+  BOOST_REQUIRE_EQUAL(dunedaq::cibmodules::util::get_m3(t), -1);
+
+  t.pos_m3 = 0x01234;
+  BOOST_REQUIRE_EQUAL(dunedaq::cibmodules::util::get_m3(t), 0x1234);
+
+  constexpr std::uint32_t m2_negative = 0x3FFFFF;
+  t.pos_m2_lsb = m2_negative & 0x7FFF;
+  t.pos_m2_msb = (m2_negative >> 15) & 0x7F;
+  BOOST_REQUIRE_EQUAL(dunedaq::cibmodules::util::get_m2(t), -1);
+
+  constexpr std::uint32_t m2_positive = 0x001234;
+  t.pos_m2_lsb = m2_positive & 0x7FFF;
+  t.pos_m2_msb = (m2_positive >> 15) & 0x7F;
+  BOOST_REQUIRE_EQUAL(dunedaq::cibmodules::util::get_m2(t), 0x1234);
 }
 
 BOOST_AUTO_TEST_CASE(ParseHexValidInputs)
@@ -29,6 +69,9 @@ BOOST_AUTO_TEST_CASE(ParseHexValidInputs)
 
   BOOST_REQUIRE(dunedaq::cibmodules::util::parse_hex("0x1A2B", value));
   BOOST_REQUIRE_EQUAL(value, 0x1A2BU);
+
+  BOOST_REQUIRE(dunedaq::cibmodules::util::parse_hex("0Xabc", value));
+  BOOST_REQUIRE_EQUAL(value, 0xABCU);
 
   BOOST_REQUIRE(dunedaq::cibmodules::util::parse_hex("ff", value));
   BOOST_REQUIRE_EQUAL(value, 0xFFU);
@@ -41,6 +84,8 @@ BOOST_AUTO_TEST_CASE(ParseHexRejectsInvalidInputs)
   BOOST_REQUIRE(!dunedaq::cibmodules::util::parse_hex("", value));
   BOOST_REQUIRE(!dunedaq::cibmodules::util::parse_hex("0x", value));
   BOOST_REQUIRE(!dunedaq::cibmodules::util::parse_hex("0x1G", value));
+  BOOST_REQUIRE(!dunedaq::cibmodules::util::parse_hex("1F ", value));
+  BOOST_REQUIRE(!dunedaq::cibmodules::util::parse_hex(" 1F", value));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
